1d41ba4a0f8b39e8b98994a8791b4ec0ba9a4675
Marcin Pająk <39952424+pelemistrz@users.noreply.github.com>
2024-10-28 08:50:46 +0100

    set up layout MainWindow

--- src/main/java/com/barosanu/Launcher.java
+++ src/main/java/com/barosanu/Launcher.java
@@ -13,9 +13,9 @@ public class Launcher extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent parent = FXMLLoader.load(getClass().getResource("/view/first.fxml"));
+        Parent parent = FXMLLoader.load(getClass().getResource("/view/MainWindow.fxml"));
 
-        Scene scene = new Scene(parent, 300, 250);
+        Scene scene = new Scene(parent);
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/main/java/com/barosanu/controller/MainWindowController.java
@@ -0,0 +1,26 @@
+package com.barosanu.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TreeView;
+import javafx.scene.web.WebView;
+
+public class MainWindowController {
+
+    @FXML
+    private WebView emailWebView;
+
+    @FXML
+    private TreeView<?> emailsTreeView;
+    @FXML
+    private TableView<?> emailsTableView;
+
+    @FXML
+    void optionsAction() {
+
+    }
+
+}
+
+
