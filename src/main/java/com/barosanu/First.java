7a08d36f71b328f8dab60ba715bc90f0bb25a995
Marcin Pająk <39952424+pelemistrz@users.noreply.github.com>
2024-10-28 08:10:00 +0100

    Set up app

---
+++ src/main/java/com/barosanu/First.java
@@ -0,0 +1,10 @@
+package com.barosanu;
+
+import javafx.fxml.FXML;
+
+public class First {
+    @FXML
+    public void buttonAction( ) {
+        System.out.println("clicked");
+    }
+}
--- src/main/java/com/barosanu/Launcher.java
+++ src/main/java/com/barosanu/Launcher.java
@@ -1,24 +1,21 @@
 package com.barosanu;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Launcher extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+
     @Override
     public void start(Stage stage) throws Exception {
-        Button button = new Button("Click!");
-        button.setOnAction(e -> {
-            System.out.println("Button clicked!");
-        });
-        StackPane stackPane = new StackPane();
-        stackPane.getChildren().add(button);
-        Scene scene = new Scene(stackPane, 300, 250);
+        Parent parent = FXMLLoader.load(getClass().getResource("/view/first.fxml"));
+
+        Scene scene = new Scene(parent, 300, 250);
         stage.setScene(scene);
         stage.show();
     }
