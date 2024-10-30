270d967d3b0207ad597f095bfe40c2b675245085
Marcin Pająk <39952424+pelemistrz@users.noreply.github.com>
2024-10-30 08:27:46 +0100

    encoding i decoding passwords

--- src/main/java/com/barosanu/controller/persistance/PersistanceAccess.java
+++ src/main/java/com/barosanu/controller/persistance/PersistanceAccess.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 public class PersistanceAccess {
    private String VALID_ACCOUNTS_LOCATION =System.getProperty("user.home")+File.separator+ "validAccounts.ser";
+    private Encoder encoder = new Encoder();
 
     public List<ValidAccount> loadFromPersistence(){
         List<ValidAccount> resultList = new ArrayList<>();
@@ -13,6 +14,7 @@ public class PersistanceAccess {
             FileInputStream fileInputStream = new FileInputStream(VALID_ACCOUNTS_LOCATION);
             ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
             List<ValidAccount> persistedList = (List<ValidAccount>) objectInputStream.readObject();
+            decodePasswords(persistedList);
             resultList.addAll(persistedList);
 
         }catch (Exception e){
@@ -20,17 +22,32 @@ public class PersistanceAccess {
         }
         return resultList;
     }
+
     public void saveToPersistence(List<ValidAccount> validAccounts){
         try{
             File file = new File(VALID_ACCOUNTS_LOCATION);
             FileOutputStream fileOutputStream = new FileOutputStream(file);
             ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+            encodePasswords(validAccounts);
             objectOutputStream.writeObject(validAccounts);
             objectOutputStream.close();
             fileOutputStream.close();
         }catch (Exception e){
             e.printStackTrace();
         }
+    }
 
+    private void decodePasswords(List<ValidAccount> persistedList) {
+        for(ValidAccount validAccount : persistedList){
+            String originalPassword = validAccount.getPassword();
+            validAccount.setPassword(encoder.decode(originalPassword));
+        }
+    }
+
+    private void encodePasswords(List<ValidAccount> persistedList) {
+        for(ValidAccount validAccount : persistedList){
+            String originalPassword = validAccount.getPassword();
+            validAccount.setPassword(encoder.encode(originalPassword));
+        }
     }
 }
